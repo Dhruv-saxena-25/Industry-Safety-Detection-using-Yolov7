fedfae0e0836fcf9216a80d79c16e41bfc55d867
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-04-12 20:05:09 +0100

    data_validation

--- isd/constant/training_pipeline/__init__.py
+++ isd/constant/training_pipeline/__init__.py
@@ -18,11 +18,11 @@ DATA_BUCKET_NAME = "isd-data-24"
 # Data Validation realted contant start with DATA_VALIDATION VAR NAME
 # """
 
-# DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
 
-# DATA_VALIDATION_STATUS_FILE = 'status.txt'
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
 
-# DATA_VALIDATION_ALL_REQUIRED_FILES = ["images", "labels", "classes.names", "train.txt", "val.txt"]
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["images", "labels", "classes.names", "train.txt", "val.txt"]
 
 
 
